63e7157b48164c319cf6ea60f5b7c5be505a2531
Vishal Alpe <110710721+Vish-001@users.noreply.github.com>
2025-07-29 18:03:41 +0530

    Delete shuttleshadow directory

--- shuttleshadow/src/main/java/com/shuttleshadow/entities/Mode.java
+++
@@ -1,8 +0,0 @@
-package com.shuttleshadow.entities;
-
-public enum Mode {
-    EASY,
-    MEDIUM,
-    HARD,
-    IMPROVE
-}
--- shuttleshadow/src/main/java/com/shuttleshadow/entities/Zone.java
+++
@@ -1,9 +0,0 @@
-package com.shuttleshadow.entities;
-
-public enum Zone {
-    TOP_LEFT,
-    TOP_RIGHT,
-    BOTTOM_LEFT,
-    BOTTOM_RIGHT,
-    CENTER
-}
